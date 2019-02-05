84afb5b6644d54f88c8e6af3c0ece9b5e249c08d
Sergey Zhmura <zhmura@gmail.com>
2019-02-05 13:51:10 +0300

    Initial commit

---
+++ src/main/java/com/globant/internal/oncocureassist/OncocureassistApplication.java
@@ -0,0 +1,14 @@
+package com.globant.internal.oncocureassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OncocureassistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OncocureassistApplication.class, args);
+    }
+
+}
+
